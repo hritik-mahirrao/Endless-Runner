d8f4b547e20a47efcca7242cfec57cdd43f8054a
Hritik Mahirrao <hritiknarendra@gmail.com>
2018-08-16 14:03:38 +0530

    Initial Template commit
    Generic Template with initial folders

---
+++ Endless Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Hspeed, VSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * Hspeed, 0, VSpeed) * Time.deltaTime;
+    }
+}
